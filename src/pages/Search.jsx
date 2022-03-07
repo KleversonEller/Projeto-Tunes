f95c612ad122b6b4980c29260301f4dedaf2c1d1
Kleverson Eller <kleverson.a16@gmail.com>
2022-03-07 20:29:28 -0300

    Requisito - 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,44 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      disable: true,
+    };
+    this.validateBtn = this.validateBtn.bind(this);
+  }
+
+  validateBtn(event) {
+    return event.target.value.length >= 2
+      ? this.setState({
+        disable: false,
+      })
+      : this.setState({
+        disable: true,
+      });
+  }
+
   render() {
+    const { disable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            name="disable"
+            data-testid="search-artist-input"
+            type="text"
+            onChange={ this.validateBtn }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ disable }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
