d977cb5f74a9935e0b1957107ae5b59f680a1171
Kleverson Eller <kleverson.a16@gmail.com>
2022-05-20 22:09:09 -0300

    :lipstick: feat: Estilizaçao CSS da pagina 'Not Found'

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import style from '../css/NotFound.module.css';
 
 class NotFound extends React.Component {
   render() {
     return (
-      <div data-testid="page-not-found" />
+      <div className={ style.container } data-testid="page-not-found">
+        <span> Não foi possível encontrar a página que você está procurando.</span>
+        <span>
+          Por favor, volte para a página inicial do
+          <Link className={ style.link } to="/search"> Trybe Tunes</Link>
+        </span>
+      </div>
     );
   }
 }
